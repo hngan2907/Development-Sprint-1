b15fa6e3d4adaf82e4867db1acceaeb0fcfba181
hngan2907 <48574063+hngan2907@users.noreply.github.com>
2019-05-17 22:07:54 +0930

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+( function( $ ) {
+$( document ).ready(function() {
+$(document).ready(function(){
+
+$('.sidenav > ul > li ul').each(function(index, e){
+  var count = $(e).find('li').length;
+  var content = '<span class=\"cnt\">' + count + '</span>';
+  $(e).closest('li').children('a').append(content);
+});
+$('.sidenav ul ul li:odd').addClass('odd');
+$('.sidenav ul ul li:even').addClass('even');
+$('.sidenav > ul > li > a').click(function() {
+  $('.sidenav li').removeClass('active');
+  $(this).closest('li').addClass('active');
+  var checkElement = $(this).next();
+  if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+    $(this).closest('li').removeClass('active');
+    checkElement.slideUp('normal');
+  }
+  if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+    $('.sidenav ul ul:visible').slideUp('normal');
+    checkElement.slideDown('normal');
+  }
+  if($(this).closest('li').find('ul').children().length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+});
+
+});
+} )( jQuery );
